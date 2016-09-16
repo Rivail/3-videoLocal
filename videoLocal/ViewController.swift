ccc6f0957ad9b3ac029c66bb8c55b4f72aaf6eaa
Rivail Menezes <menezes.177@hotmail.com>
2016-09-16 17:51:03 -0300

    videoLocal

--- videoLocal/ViewController.swift
+++ videoLocal/ViewController.swift
@@ -7,9 +7,23 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
-
+    var moviePlayer: MPMoviePlayerViewController?
+    @IBAction func clicaVideo(sender: AnyObject) {
+        
+        let path = NSBundle.mainBundle().pathForResource("The Greatest Bass Solo Ever", ofType: "mp4")
+        let url = NSURL.fileURLWithPath(path!)
+         moviePlayer =  MPMoviePlayerViewController(contentURL: url)
+        
+        if let player = moviePlayer?.moviePlayer {                               //Está dando erro aqui
+            player.view.frame = self.view.bounds
+            player.prepareToPlay()
+            player.scalingMode = .AspectFill
+            self.view.addSubview(player.view)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
